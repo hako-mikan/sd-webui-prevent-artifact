95eb892513ba10c47d7da24bd5f89cf07edd056b
hako-mikan <122196982+hako-mikan@users.noreply.github.com>
2024-02-09 21:04:11 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Prevent Artifact
+This is an auxiliary script for the Stable Diffusion Web-UI. It prevents image corruption that occurs in the SDXL series.
+Stable Diffusion Web-UI用の補助スクリプトです。SDXL系統で発生する画像の破綻を防ぎます。
+
+# Overview/概要
+This script prevents the corruption of generated images that started being reported around the time animagine-V3 became popular. For more details, please refer [here](https://civitai.com/articles/4044).
+animagine-V3が流行りだした頃から報告され始めた生成画像の破綻を防ぐスクリプトです。詳しくは[こちら]()を参照して下さい。
+
+# Install/インストール
+`https://github.com/hako-mikan/sd-webui-prevent-artifact`
+After installing from Install from URL of Web-UI, please restart.
+Web-UIのInstall from URLからインストールした後、再起動して下さい。
+
+# How to Use/使い方
+After installing and restarting, an option called Prevent Artifact will appear in Settings. By checking the box for Disable taking the average value when calculating cond/uncond and saving, it will be activated.
+インストールして再起動すると、SettingsにPrevent Artifactという項目が現れます。Disable taking the average value when calculating cond/uncondにチェックを入れ保存すると有効化します。
---
+++ scripts/pa.py
@@ -0,0 +1,57 @@
+import torch
+from modules import shared, devices, sd_hijack_clip
+from modules.script_callbacks import on_ui_settings
+
+OPT_NAME = "disable_mean_in_calclate_cond"
+       
+def ext_on_ui_settings():
+    # [setting_name], [default], [label], [component(blank is checkbox)], [component_args]debug_level_choices = []
+    negpip_options = [
+        (OPT_NAME, False, "Disable taking the average value when calculating cond/uncond")
+    ]
+    section = ('Prevent Artifact', "Prevent Artifact")
+
+    for cur_setting_name, *option_info in negpip_options:
+        shared.opts.add_option(cur_setting_name, shared.OptionInfo(*option_info, section=section))
+
+on_ui_settings(ext_on_ui_settings)
+
+def process_tokens(self, remade_batch_tokens, batch_multipliers):
+    """
+    sends one single prompt chunk to be encoded by transformers neural network.
+    remade_batch_tokens is a batch of tokens - a list, where every element is a list of tokens; usually
+    there are exactly 77 tokens in the list. batch_multipliers is the same but for multipliers instead of tokens.
+    Multipliers are used to give more or less weight to the outputs of transformers network. Each multiplier
+    corresponds to one token.
+    """
+    tokens = torch.asarray(remade_batch_tokens).to(devices.device)
+
+    # this is for SD2: SD1 uses the same token for padding and end of text, while SD2 uses different ones.
+    if self.id_end != self.id_pad:
+        for batch_pos in range(len(remade_batch_tokens)):
+            index = remade_batch_tokens[batch_pos].index(self.id_end)
+            tokens[batch_pos, index+1:tokens.shape[1]] = self.id_pad
+
+    z = self.encode_with_transformers(tokens)
+
+    pooled = getattr(z, 'pooled', None)
+
+    # restoring original mean is likely not correct, but it seems to work well to prevent artifacts that happen otherwise
+    batch_multipliers = torch.asarray(batch_multipliers).to(devices.device)
+    original_mean = z.mean()
+    z = z * batch_multipliers.reshape(batch_multipliers.shape + (1,)).expand(z.shape)
+    new_mean = z.mean()
+    print(original_mean,new_mean)
+    print(original_mean / new_mean)
+    if hasattr(shared.opts, OPT_NAME):
+        if not getattr(shared.opts, OPT_NAME, False):
+            z = z * (original_mean / new_mean)
+    else:
+        z = z * (original_mean / new_mean)
+
+    if pooled is not None:
+        z.pooled = pooled
+
+    return z
+
+sd_hijack_clip.FrozenCLIPEmbedderWithCustomWordsBase.process_tokens = process_tokens
